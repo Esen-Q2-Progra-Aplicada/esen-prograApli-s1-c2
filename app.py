e37f1d9127b0a03f42154bee29434ea9e3f7d48d
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-05-14 18:50:24 -0600

    primer ejercicio

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("hello world")
+# lista
+numberList = []
+print(numberList)
+
+numberList = [1, 2, 2, 4]
+print(numberList)
+print(len(numberList))
+
+numberList.append(5)
+print(numberList)
+
+numberList[0] = 0
+print(numberList)
+
+for number in numberList:
+    print(number)
+
+fruitList = ["apple", "strawberry"]
+for a in fruitList:
+    print(a)
+
+# ejercicio
+numberList = range(1, 20)
+# sacar la suma de todos los numeros
